49f3388aeed987000f69322aad0bded2a3a2babc
Lea Jasmine <leajasmine@Leas-MacBook-Pro.local>
2024-03-04 14:02:55 -0500

    added loading spinner and search functionality with clear button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
@@ -10,21 +12,34 @@ class App extends Component {
     loading: false
   }
   
-  async componentDidMount() { 
+static propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired
+}
 
+  // Search GitHub users 
+  searchUsers = async text => {
     this.setState({ loading: true });
+
     const res = await axios
-    .get('https://api.github.com/users')
+    .get(`https://api.github.com/search/users?q=${text}&client_id=${
+      process.env.REACT_APP_GHFINDER_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GHFINDER_SECRET_ID}`)
     console.log(res.data);
 
-    this.setState({ users: res.data, loading: false });
+    this.setState({ users: res.data.items, loading: false });
   }
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() { 
     return (
     <nav className='App'>
       <Navbar title='Github Finder' icon='fab fa-github'/>
       <div className="container">
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true: false} />
         <Users loading={this.state.loading} users={this.state.users}/>
       </div>
     </nav>
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+// arrow function component
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+
+export const Spinner = () => {
+    <fragment>
+        img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }}
+    </fragment>
+}
+
+export default Spinner;
